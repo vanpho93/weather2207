51af9445a13b3d111169b1d3ce3f410e1401e08f
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-26 16:19:11 +0700

    xong

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -5,16 +5,21 @@ import * as actionCreators from '../redux/action';
 import { connect } from 'react-redux';
 
 class WeatherForm extends Component {
+    onSubmitForm(e) {
+        e.preventDefault();
+        const { getWeather } = this.props;
+        getWeather(this.refs.txtCityName.value)
+    }
     render() {
         const { getWeather } = this.props;
         return (
-            <div>
+            <form onSubmit={this.onSubmitForm.bind(this)}>
                 <input type="text" placeholder="Enter your city name" ref="txtCityName"/>
                 <br /><br />
                 <button onClick={() => getWeather(this.refs.txtCityName.value)}>
                     Get Weather
                 </button>
-            </div>
+            </form>
         );
     }
 }
