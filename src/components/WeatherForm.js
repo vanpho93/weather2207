17d6b7c09f0c3dd59176ca4c9e0d3c65693f7330
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-26 15:39:36 +0700

    actioncreators

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import getWeatherByCityName from '../api/getTempByCityName';
+import { connect } from 'react-redux';
 
-export default class WeatherForm extends Component {
+class WeatherForm extends Component {
+    getWeather() {
+        const { txtCityName } = this.refs;
+        const cityName = txtCityName.value;
+        const { dispatch } = this.props;
+        dispatch({ type: 'BAT_DAU_TIM_KIEM' });
+        getWeatherByCityName(cityName)
+        .then(temp => dispatch({ type: 'TIM_KIEM_THANH_CONG', cityName, temp}))
+        .catch(error => dispatch({ type: 'TIM_KIEM_THAT_BAI', error }));
+    }
     render() {
         return (
             <div>
-                <input type="text" placeholder="Enter your city name"/>
+                <input type="text" placeholder="Enter your city name" ref="txtCityName"/>
                 <br /><br />
-                <button>Get Weather</button>
+                <button onClick={this.getWeather.bind(this)}>Get Weather</button>
             </div>
         );
     }
 }
+
+export default connect()(WeatherForm);
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,12 @@ const defaultState = {
 };
 
 const reducer = (state = defaultState, action) => {
+    const batDauTimKiem = { cityName: null, temp: null, isLoading: true, error: null};
+    const timKiemThanhCong = { cityName: action.cityName, temp: action.temp, isLoading: false, error: null};
+    const timKiemThatBai = { cityName: null, temp: null, isLoading: false, error: action.error};
+    if (action.type === 'BAT_DAU_TIM_KIEM') return batDauTimKiem;
+    if (action.type === 'TIM_KIEM_THANH_CONG') return timKiemThanhCong;
+    if (action.type === 'TIM_KIEM_THAT_BAI') return timKiemThatBai;
     return state;
 };
 
